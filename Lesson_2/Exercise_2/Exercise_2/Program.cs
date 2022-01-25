a99f002289a5a42dab9f388af227e7e9ba03fca6
Nikita <nickita.sevsk@gmail.com>
2022-01-25 16:15:08 +0300

    Exercise_2_Test

---
+++ Lesson_2/Exercise_2/Exercise_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            var s = Console.ReadLine();
+            Console.WriteLine(TurnRoundString(s));
+        }
+        public static string TurnRoundString (string s)
+        {
+            var ss = new char[s.Length];
+            var j = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                ss[j] = s[i];
+                j++;
+            }
+            StringBuilder sb = new StringBuilder(ss.Length);
+            string result = sb.Append(ss).ToString();
+            return result;
+        }
+    }
+}
