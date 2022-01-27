961e9156315ad0c1cb093340890d81438fa1ed05
Nikita <nickita.sevsk@gmail.com>
2022-01-27 15:57:37 +0300

    Exercise_1

---
+++ Lesson_3/Exercise_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Build build_1 = new Build();
+            build_1.FillInDataOfBuild();
+            build_1.PrintDataOfBuild();
+            Console.WriteLine($"Высота этажа: {build_1.HeightOfStorey()}");
+            Console.WriteLine($"Квартир в парадной: {build_1.NumOfFlatsInTheEntrance()}");
+            Console.WriteLine($"Квартир на этаже: {build_1.NumOfFlatsOnTheFloor()}");
+        }
+    }
+}
