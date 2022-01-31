6aa8ded84ced45910b3ccc8655ffcb497de1e3de
Nikita <nickita.sevsk@gmail.com>
2022-01-31 15:51:14 +0300

    Exercise_1

---
+++ Lesson_5/Irrational_numbers/IrrationalNumber.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Irrational_numbers
+{
+    public class IrrationalNumber
+    {
+        private int _Numerator;
+        private int _Denominator;
+        public int Numerator => _Numerator;
+        
+        public int Denominator => _Denominator;
+       
+        public IrrationalNumber (int numerator, int denominator)
+        {
+            _Denominator = denominator;
+            _Numerator = numerator;
+        }
+        public void PrintRationalNum()
+        {
+            Console.WriteLine("{0:0.#####}", (double)_Numerator / (double)_Denominator);
+        }
+        public static IrrationalNumber operator +(IrrationalNumber num1, IrrationalNumber num2)
+        {
+
+            return new IrrationalNumber ((num2._Denominator * num1._Numerator) + (num1._Denominator * num2._Numerator), num1._Denominator * num2._Denominator);
+        }
+        public static IrrationalNumber operator -(IrrationalNumber num1, IrrationalNumber num2)
+        {
+            return new IrrationalNumber((num2._Denominator * num1._Numerator) - (num1._Denominator * num2._Numerator), num1._Denominator * num2._Denominator);
+        }
+        public static IrrationalNumber operator *(IrrationalNumber num1, IrrationalNumber num2)
+        {
+            return new IrrationalNumber(num1._Numerator * num2._Numerator, num1._Denominator * num2._Denominator);
+        }
+        public static IrrationalNumber operator /(IrrationalNumber num1, IrrationalNumber num2)
+        {
+            return new IrrationalNumber(num1._Numerator * num2._Denominator, num1._Denominator * num2._Numerator);
+        }
+        public static IrrationalNumber operator %(IrrationalNumber num1, IrrationalNumber num2)
+        {
+            return new IrrationalNumber((num1._Numerator * num2._Denominator) % (num1._Denominator * num2._Numerator), 1);
+        }
+        public static bool operator ==(IrrationalNumber num1, IrrationalNumber num2)
+        {
+            return (((double)num1._Numerator / (double)num1._Denominator) == ((double)num2._Numerator / (double)num2._Denominator));
+        }
+        public static bool operator !=(IrrationalNumber num1, IrrationalNumber num2)
+        {
+            return (((double)num1._Numerator / (double)num1._Denominator) != ((double)num2._Numerator / (double)num2._Denominator));
+        }
+        public static bool operator >(IrrationalNumber num1, IrrationalNumber num2)
+        {
+            return (((double)num1._Numerator / (double)num1._Denominator) > ((double)num2._Numerator / (double)num2._Denominator));
+        }
+        public static bool operator <(IrrationalNumber num1, IrrationalNumber num2)
+        {
+            return (((double)num1._Numerator / (double)num1._Denominator) < ((double)num2._Numerator / (double)num2._Denominator));
+        }
+        public static bool operator >=(IrrationalNumber num1, IrrationalNumber num2)
+        {
+            return (((double)num1._Numerator / (double)num1._Denominator) >= ((double)num2._Numerator / (double)num2._Denominator));
+        }
+        public static bool operator <=(IrrationalNumber num1, IrrationalNumber num2)
+        {
+            return (((double)num1._Numerator / (double)num1._Denominator) <= ((double)num2._Numerator / (double)num2._Denominator));
+        }
+        public static IrrationalNumber operator ++(IrrationalNumber num1)
+        {
+            return new IrrationalNumber(num1._Numerator + num1._Denominator, num1._Denominator);
+        }
+        public static IrrationalNumber operator --(IrrationalNumber num1)
+        {
+            if (num1._Numerator > num1._Denominator)
+                return new IrrationalNumber(num1._Numerator - num1._Denominator, num1._Denominator);
+            return new IrrationalNumber(-(num1._Denominator - num1._Numerator), num1._Denominator);
+        }
+        public static implicit operator float(IrrationalNumber num1)
+        {
+            return (float)num1._Numerator / (float)num1._Denominator;
+        }
+        public static implicit operator int(IrrationalNumber num1)
+        {
+            return num1._Numerator / num1._Denominator;
+        }
+        public override string ToString()
+        {
+            return ((double)_Numerator / (double)_Denominator).ToString();
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is IrrationalNumber other_num)
+                return ((double)_Numerator / (double)_Denominator) == ((double)other_num._Numerator / (double)other_num._Denominator);
+            return false;
+        }
+    }
+}
---
+++ Lesson_5/Lesson_5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Irrational_numbers;
+
+namespace Lesson_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num1 = new IrrationalNumber(12, 34);
+            var num2 = new IrrationalNumber(5, 8);
+            var result = num1 + num2;
+            var result_1 = num1 - num2;
+            var result_2 = num1 * num2;
+            var result_3 = num1 / num2;
+            var result_4 = num1 % num2;
+            var result_5 = num1 == num2;
+            var result_6 = num1 != num2;
+            var result_7 = num1 > num2;
+            var result_8 = num1 < num2;
+            var result_9 = num1++;
+            var result_10 = num2--;
+            var result_11 = (float)num1;
+            var result_12 = (int)num1;
+            result.PrintRationalNum();
+            result_1.PrintRationalNum();
+            result_2.PrintRationalNum();
+            result_3.PrintRationalNum();
+            result_4.PrintRationalNum();
+            Console.WriteLine(result_5);
+            Console.WriteLine(result_6);
+            Console.WriteLine(result_7);
+            Console.WriteLine(result_8);
+            result_9.PrintRationalNum();
+            result_10.PrintRationalNum();
+            Console.WriteLine(result_11);
+            Console.WriteLine(result_12);
+            Console.WriteLine(num1.ToString());
+            Console.WriteLine(num1.Equals(num2));
+        }
+    }
+}
