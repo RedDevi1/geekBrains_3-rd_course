2b2bc9cbaaccd4cb230cc1ea5ffbaecfe589e343
Nikita <nickita.sevsk@gmail.com>
2022-02-04 15:48:51 +0300

    Lesson_6_Exercise_1_Begining

---
+++ Lesson_7/Lesson_7/BCoder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7
+{
+    class BCoder : ICoder
+    {
+        public string Encode(string filePath)
+        {
+            return;
+        }
+        public string Decode(string filePath)
+        {
+            return;
+        }
+    }
+}
